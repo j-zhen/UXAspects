e212df3a3e2b5f66960c89b6d0a58aa04c2c0efc
Alastair McKee <alastair.ian.mckee@hpe.com>
2017-05-19 16:33:34 +0100

    EL-2483 - in progress

--- src/components/item-display-panel/item-display-panel.component.ts
+++ src/components/item-display-panel/item-display-panel.component.ts
@@ -1,25 +1,60 @@
-import { Component, Directive, Input, SimpleChange } from '@angular/core';
+import { Component, Directive, Input, SimpleChange, ViewChild, ElementRef } from '@angular/core';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
     selector: 'ux-item-display-panel',
     templateUrl: './item-display-panel.component.html',
-    exportAs: 'ux-item-display-panel'
+    exportAs: 'ux-item-display-panel',
+    host: {
+        '(document:click)': 'clickOff($event)',
+        '(document:keyup.escape)': 'hide()'
+    }
 })
 export class ItemDisplayPanelComponent { 
     @Input() top: number;
     @Input() shadow: boolean;
     @Input() visible: boolean;
     @Input() title: string;
+    @Input() animate: boolean;
 
     height: string;
+    
+    constructor(private elementRef: ElementRef) {
+    }
+   
 
     ngOnChanges(changes: {[top: number]: SimpleChange}) {
         this.height = 'calc(100% - ' + this.top + 'px)';
     }
 
-    show() {
+    clickOff(event: any) {
+       
+        // dont do anything if the panel is hidden
+        if (this.visible) {
+            let target = event.target;
+
+            let hidePanel = true;
+
+            while (target.nodeName !== 'BODY') {
+                if (target.classList.contains('ux-item-display-panel')) {
+                    hidePanel = false;
+                    break;
+                } else {
+                    target = target.parentNode;
+                }
+            }
+
+            if (hidePanel) {
+                this.hide();
+            }
+        }
+    }
+
+    show(event: MouseEvent) {
+        
+        event.stopPropagation();
+
         if (!this.visible) {
             this.visible = true;
         }
